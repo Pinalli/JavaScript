13a6c6990dfcd6091c42a9cc7928815426da9edb
Pinalli <betopinalli@gmail.com>
2020-02-08 11:50:21 -0300

    Condicionais

---
+++ aula11/exe008.js
@@ -0,0 +1,9 @@
+var vel = 61
+console.log(`A velo do seu carro é ${vel} Km/h `)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO! `)
+}
+else{
+    console.log(`Parabéns! você está com a velocidade correta `)
+}
+console.log(`Dirija sempre usando cinto de segurança! `)
