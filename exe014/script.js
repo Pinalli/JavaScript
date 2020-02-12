b26329813d888b027c484bd89efcbf88d04579ee
Pinalli <betopinalli@gmail.com>
2020-02-12 18:31:50 -0300

    Verificador de idade

--- exe014/script.js
+++ exe014/script.js
@@ -2,11 +2,12 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = 19   //  data.getHours()
+    var hora = 13   //  data.getHours()
+    var minutos = data.getMinutes
     msg.innerHTML = `<strong>Agora são ${hora} horas!</strong>`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
-        document.body.style.background = '#e8cd7f'
+        document.body.style.background = '#f7ebdd'
     }else if (hora >= 12 && hora <= 18) {
         img.src = 'fototarde.png'
         document.body.style.background = '#fdc18a'
